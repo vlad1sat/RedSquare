7a5275893953985710ae642c845bea03708244bb
vlad1sat <pyatkov200316@mail.ru>
2023-01-29 22:50:37 +0500

    create: logic program

--- redSquares.js
+++ redSquares.js
@@ -1,10 +1,29 @@
-function getValueForm() {
-    const form = document.querySelector('.form-square');
-    let number = 4;
-    form.addEventListener('submit', () => {
-        const input = form.querySelector('#input-square');
-        number = input.value;
-    });
-    return number;
+
+function createSquare() {
+    const square = document.createElement('div');
+    square.classList.add('styleCard');
+    return square;
 }
 
+function logicApp() {
+    const form = document.querySelector('.form-square');
+    let countSquare = 4;
+    form.addEventListener('submit',evt => {
+        evt.preventDefault();
+        const input = form.querySelector('#input-square');
+        countSquare = +input.value;
+        const area = document.getElementById('squares');
+        area.replaceChildren();
+        for (let indexSquare = 0; indexSquare < countSquare; indexSquare++) {
+            let square = createSquare();
+            area.appendChild(square);
+            square.addEventListener('click', () => {
+                const color = window.getComputedStyle(square).backgroundColor;
+                if (color === 'rgb(255, 0, 0)') square.style.backgroundColor = 'green';
+                else square.style.backgroundColor = 'red';
+            })
+        }
+    });
+}
+
+logicApp();
